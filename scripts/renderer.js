3695b90b26702387e5c71e7a47e1485748f6e169
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2025-03-04 12:43:30 +0100

    Drag & drop wip

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,5 +1,7 @@
 //Este script es el que se ejecuta en el proceso de renderizado.
 //Se puede usar para acceder a funciones o variables del proceso principal y mostrarlas.
+
+//Prueba
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
@@ -10,3 +12,31 @@ const func = async () => {
   }
   
 func()
+
+
+//Drag&Drop
+const dropArea = document.getElementById('drop-area');
+const fileInput = document.getElementById('file-input');
+
+//Funcion para evitar el comportamiento por defecto del "navegador"
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+dropArea.addEventListener('dragover', preventDefaults);
+dropArea.addEventListener('dragenter', preventDefaults);
+dropArea.addEventListener('dragleave', preventDefaults);
+
+
+// Manejar comportamiento cuando dropear un archivo
+dropArea.addEventListener('drop', handleDrop);
+
+//Cambia el estilo del css cuando pones un archivo encima
+dropArea.addEventListener('dragover', () => {
+    dropArea.classList.add('drag-over');
+});
+  
+  dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('drag-over');
+});
