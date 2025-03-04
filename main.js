711ac2df7ef49ee7bd103026e5db7db2d7eae369
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2025-03-04 11:31:40 +0100

    Comentarios y preload

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
+//Imports
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
+
+//Configura la ventana principal
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
-    height: 650
+    height: 650,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('routes/index.html')
 }
 
+/*
+//Cuando la aplicación esté lista, crea una ventana
 app.whenReady().then(() => {
   createWindow()
 })
+*/
+
+//Crear la ventana pero la comprobación extra es para macOS
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+})
+
+//Asegurarse de que se cierra la aplicación cuando todas las ventanas están cerradas (excepto en macOS)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
